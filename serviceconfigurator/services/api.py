866af71acbc925a45dcc265e785c2ea60160f758
David Meister <david_meister@bluewin.ch>
2020-08-19 19:42:20 +0200

    create basic api

---
+++ serviceconfigurator/services/api.py
@@ -0,0 +1,10 @@
+from services.models import Service
+from rest_framework import viewsets, permissions
+from .serializers import ServiceSerializer
+
+class ServiceViewSet(viewsets.ModelViewSet):
+    queryset = Service.objects.all()
+    permission_classes = [
+        permissions.AllowAny
+    ]
+    serializer_class = ServiceSerializer
