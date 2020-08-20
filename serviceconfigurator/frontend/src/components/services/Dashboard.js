3ffc5878ac5fede4f95426f11e92968c0b090499
David Meister <david_meister@bluewin.ch>
2020-08-20 10:58:56 +0200

    first react pages

---
+++ serviceconfigurator/frontend/src/components/services/Dashboard.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+import Form from "./Form";
+import Services from "./Services";
+
+export default function Dashboard() {
+  return (
+    <Fragment>
+      <Form />
+      <Services />
+    </Fragment>
+  );
+}
